27cfbdfd10bed6df330cb8852211c9891ac0e35f
Parker Jones <parker.harold.jones@gmail.com>
2020-12-27 19:52:45 -0700

    Made working delete and edit and return functions

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -7,11 +7,35 @@ class CoachesController < ApplicationController
     @coach = Coach.find(params[:id])
   end
 
-  def new 
-    @coach = Coach.new
-  end 
+  # def new 
+  #   @coach = Coach.new
+  # end 
 
   def edit 
     @coach = Coach.find(params[:id])
   end 
+
+  def update
+    @coach = Coach.find(params[:id])
+    if @coach.update(coach_params)
+      redirect_to @coach
+    else 
+      render :edit 
+    end 
+  end 
+  
+  def destroy
+    @coach = Coach.find(params[:id])
+    @coach.destroy
+    redirect_to coaches_path
+  end 
+
+
+  private 
+  
+  def coach_params
+    params.require(:coach).permit(:name, :college, :age)
+  end 
+
+  
 end
