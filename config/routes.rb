f7b061c4cf1d81e6b5e798331a79fb26d63eaae7
Parker Jones <parker.harold.jones@gmail.com>
2021-01-18 11:55:54 -0700

    Ready to push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "coaches#index"
 resources :coaches
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+end
+get '*other', to: 'static#index'
 end
