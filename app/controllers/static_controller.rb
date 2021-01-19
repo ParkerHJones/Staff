ffa7bd9ed661050cdd31c84b5315b3c92af96ee9
Parker Jones <parker.harold.jones@gmail.com>
2021-01-18 17:08:38 -0700

    Updated Procfile

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,4 @@ class StaticController < Rails::ApplicationController
   def index
     render file: Rails.root.join('public', 'index.html')
   end
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "coaches#index"
 resources :coaches
-  
+
 end
-get '*other', to: 'static#index'
-end
+
